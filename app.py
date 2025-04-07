c63c6ed308e73f22ce069a5fb6da20f01b092dd9
Annika Brundyn <annikabrundyn1@gmail.com>
2025-04-06 18:15:57 -0700

    fix bind flask app for render deploy

--- app.py
+++ app.py
@@ -62,4 +62,4 @@ def incoming_message():
     return str(resp)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
